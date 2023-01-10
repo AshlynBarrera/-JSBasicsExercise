3420976b83c131d44d629eeb5aa0d3411c92da6d
DaySlacker <111376118+AshlynBarrera@users.noreply.github.com>
2023-01-10 10:32:28 -0600

    Created new file script.js

---
+++ script.js
@@ -0,0 +1,37 @@
+
+
+
+
+
+// 1. lucky numbers 
+console.log("2457");
+
+// 2. Data Types 
+// Numbers, Strings, Boolean, Null,& Undefined 
+
+// 3.
+//(Parenthesis () , Exponents ** , Multiplication * , Division / , Modulus % , Addition + , & Subtraction -)
+
+// 4.
+console.log(5 % 4);
+console.log(6 % 5);
+
+// 5. 
+console.log('I\'M MARY POPPINS, Y\'ALL!');
+
+// 6.
+ console.log( 'Hawk' + 'Eye');
+
+// 7. 
+console.log('Ashlyn'.length + 'Barrera'.length);
+
+// 8. 
+console.log('Avengers: Endgame'[16]);
+
+
+//BONUS
+
+// 1. 
+console.log('/');
+
+//2.
